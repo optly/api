846118478209497118a4cbe0f96f6d36a3ca02fb
Brian <bkenny1123@hotmail.com>
2017-01-16 21:32:21 -0600

    yo i changed the message

--- src/api/core.clj
+++ src/api/core.clj
@@ -7,4 +7,4 @@
   [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Welcome to Optly beta.  Optly will help you <strong>optimize</strong> your time with a cutting edge todo list manager."})
+   :body "Welcome to Optly beta.  Optly will help you <strong>optimize</strong> your time with a cutting edge todo list manager. Don't be a loser use our app! "})
