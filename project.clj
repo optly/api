22dd1923b8f24720dd5ee423a827006e281d6692
John McConnell <johnnyillinois@gmail.com>
2017-01-23 23:02:29 -0800

    Fix: lein deployment for heroku

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject api "0.1.0-SNAPSHOT"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.6.1"
   :description "Optly API"
   :url "https://github.com/optly/api"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,8 @@
             [lein-ring "0.10.0"]
             [lein-codox "0.10.2"]
             [drift "1.5.3"]]
-  :dependencies [[org.clojure/tools.trace "0.7.9"]
+  :dependencies [[drift "1.5.3"]
+                 [org.clojure/tools.trace "0.7.9"]
                  [org.postgresql/postgresql "9.4.1212.jre7"]
                  [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
