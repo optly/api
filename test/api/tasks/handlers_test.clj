5f7a3c0e7a7e7884c9be935d2f5090aa2183d189
John McConnell <johnnyillinois@gmail.com>
2017-02-05 20:23:21 -0800

    Implemented adding task creation

--- test/api/tasks/handlers_test.clj
+++ test/api/tasks/handlers_test.clj
@@ -7,14 +7,23 @@
    [api.db.utils :as db-utils]
    [config.db :refer [connection]]
    [api.tasks.generators :as gen]
-   [api.http.mock-helpers :refer [api-get]]
-   [ring.util.http-predicates :refer [ok?]]
+   [api.http.mock-helpers :refer
+    [api-get api-post with-body]]
+   [ring.util.http-predicates :refer [ok? created?]]
    [cheshire.core :refer [parse-string]]
    [clojure.test :refer [deftest testing is]]
    [clojure.test.check.properties :refer [for-all]]
    [clojure.test.check.clojure-test :refer [defspec]]))
 
-(defn read-json
+(defn read-task-json
+  [body]
+  (->
+   body
+   (parse-string true)
+   (update :created_at from-string)
+   (update :updated_at from-string)))
+
+(defn read-tasks-json
   [body]
   (->>
    (parse-string body true)
@@ -33,6 +42,20 @@
                            (api-get :tasks)
                            handler)
                  body (slurp (response :body))
-                 json (read-json body)]
+                 json (read-tasks-json body)]
              (is (true? (ok? response)))
              (is (= [task] json)))))
+
+(defspec create-api-tasks-handler-test
+  20
+  (for-all [params gen/create-params]
+           (db-utils/delete-all :tasks (connection))
+           (let [response (->
+                           (api-post :tasks)
+                           (with-body params)
+                           handler)
+                 task (-> (db/select!) first)
+                 body (slurp (response :body))
+                 json (read-task-json body)]
+             (is (true? (created? response)))
+             (is (= task json)))))
